c4424df9b5569ec0cbba8fa2771fbf613a53bb76
Lance Hirsch <lxirsh@gmail.com>
2015-05-16 11:42:21 +0300

    Create button and function to display an alert.

--- AlertViewController/ViewController.swift
+++ AlertViewController/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var AlertViewController: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func AlertViewController(sender: AnyObject) {
+        
+        let nextController = UIAlertController()
+        let okAction = UIAlertAction (title: "ok", style: UIAlertActionStyle.Default){ action in self.dismissViewControllerAnimated(true, completion:nil)
+        }
+        nextController.addAction(okAction)
+        self.presentViewController(nextController, animated: true, completion: nil)
     }
-
 
 }
 
